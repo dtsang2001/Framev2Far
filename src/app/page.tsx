fd99d16b580e0c90684d60c9941796725e2d4334
Đinh Thế Sang <thesang201@gmail.com>
2024-12-12 16:06:37 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const appUrl = process.env.NEXT_PUBLIC_URL;
 
 const frame = {
   version: "next",
-  imageUrl: `${appUrl}/opengraph-image`,
+  imageUrl: `${appUrl}/background.jpg`,
   button: {
     title: "Play 2048 in Farcaster",
     action: {
