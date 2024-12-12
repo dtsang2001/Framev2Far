47937d16502ff71be522fef21e0cff56769b51b9
Đinh Thế Sang <thesang201@gmail.com>
2024-12-12 17:28:35 +0700

    update

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -8,12 +8,13 @@ export async function GET() {
       "signature": "MHgxMzg3Nzk1MjliZWNjYzMwOWJkZmFiNGZkNzE3ODg4NWU1OTk5NjUxYjQ5NDNlYWQ4YTA1ODAyNDcyNTBhNmVlNGJiNGFmZWQ1YjQ1ZmYyY2I4MTE5OWQ0YjQ0YzBlMTA5OWNkZDJhMDEyNTVmYWNjNmM0MWZjNTI2YTQ4YmE2MTFi"
     },
     frame: {
-      version: "0.1.3",
+      version: "0.0.1",
       name: "Play 2048 in Farcaster",
       iconUrl: `${appUrl}/icon.png`,
       splashImageUrl: `${appUrl}/splash.png`,
       splashBackgroundColor: "#8e71a7",
-      homeUrl: appUrl
+      homeUrl: appUrl,
+      webhookUrl: `${appUrl}/webhook`
     },
   };
 
